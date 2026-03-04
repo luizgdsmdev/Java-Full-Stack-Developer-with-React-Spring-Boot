bde78b598f42575ad4cea7a7d2299d8694e6dde4
Luiz Messias <luizgdsmdev@gmail.com>
2026-03-04 20:08:59 -0300

    Fix: Update product fetching hook
    Refactors the product fetching hook to a new file name for better organization.
    Adjusts the `SortByInput` component's max-width for improved styling.
    Removes commented-out code and unused imports in `Home.jsx` to clean up the component.
    </body >

--- section-1/Ystore-ui/src/components/homePage/Home.jsx
+++ section-1/Ystore-ui/src/components/homePage/Home.jsx
@@ -1,13 +1,11 @@
-// src/components/Home.tsx (ou onde estiver)
 import styles from "./Home.module.css";
 import PageHeader from "../shared/pageHeader/PageHeader";
 import ProductListing from "../products/productListing/productListing";
 import Loading from "../shared/loading/Loading";
 import Error from "../shared/error/Error";
 import NoProductsMessage from "../products/productListing/NoProductsMessage";
-// import SearchBox from "../searchBox/SearchTextInput/SearchTextInput"; // Comente se não usar
 
-import { useProducts } from "../../hooks/products/useProducts";
+import { useProducts } from "../../hooks/products/productsQueryFetch";
 
 const Home = () => {
   const { data: products = [], isLoading, isError, error } = useProducts();
@@ -28,7 +26,6 @@ const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <PageHeader />
-      {/* <SearchBox /> */} {/* Descomente se usar */}
       <ProductListing products={products} />
     </div>
   );
